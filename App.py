1de02b13762ec2591a0195ee90cb169dbe62ea48
Cristian <128819507+fremencl@users.noreply.github.com>
2024-02-27 11:50:43 -0300

    Update App.py
    Incluyendo 8859-1 y ";" como separador

--- App.py
+++ App.py
@@ -10,12 +10,20 @@ archivo_usuario = st.file_uploader("Por favor, cargue su archivo de datos en for
 
 # Verificación y carga del DataFrame
 if archivo_usuario is not None:
-    df = pd.read_csv(archivo_usuario)
-    
-    # Mostramos un preview del DataFrame para confirmación del usuario
-    st.write(df)
+    try:
+        # Especificamos la codificación ISO-8859-1 y el separador ";"
+        df = pd.read_csv(archivo_usuario, encoding='ISO-8859-1', sep=';')
+    except UnicodeDecodeError as e:
+        st.error("Error de decodificación. Intente cambiar la codificación del archivo si el problema persiste.")
+        raise e
+    except Exception as e:
+        st.error(f"Se ha producido un error al cargar el archivo: {e}")
+        raise e
+    else:
+        # Mostramos un preview del DataFrame para confirmación del usuario
+        st.write(df)
 
-    # Notificación de carga completada
-    st.success("Archivo cargado exitosamente!")
+        # Notificación de carga completada
+        st.success("Archivo cargado exitosamente!")
 else:
     st.write("Por favor, cargue un archivo CSV para continuar.")
