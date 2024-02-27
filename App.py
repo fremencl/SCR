bf03be997a183f1a1fc9f9c71bf2cf66e1ea8aef
Cristian <128819507+fremencl@users.noreply.github.com>
2024-02-27 12:48:31 -0300

    Update App.py
    Ya nos pusimos grandes. Agregamos nuevas columnas, eliminamos NOEJ e incluimos boton de descarga

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 # Título de la aplicación
-st.title('Portal Procesamiento datos SCR')
+st.title('Portal Procesamientos datos SCR')
 
 # Carga del archivo - solo CSV
 archivo_usuario = st.file_uploader("Por favor, cargue su archivo de datos en formato CSV aquí", type=['csv'])
@@ -17,10 +17,19 @@ if archivo_usuario is not None:
         # Transformamos la columna "Orden" a string
         df['Orden'] = df['Orden'].astype(str)
         
-        # Convertimos las columnas de fechas de formato Excel a formato de fecha
-        for col in ['Fe.inic.extrema', 'Fecha entrada']:
+        # Convertimos las columnas de fechas de formato Excel a formato de fecha y eliminamos la hora
+        for col in ['Fe.inic.extrema', 'Fe.fin.extrema', 'Fecha entrada']:
             df[col] = pd.to_datetime(df[col], origin='1899-12-30', unit='D').dt.date
-            
+        
+        # Agregamos las nuevas columnas
+        df["CODIGO_OBRA"] = pd.NA
+        df["RECINTO"] = pd.NA
+        df["LOCALIDAD"] = pd.NA
+        df["TIPO_OBRA"] = pd.NA
+
+        # Eliminamos las filas donde "Status usuario" es igual a "NOEJ"
+        df = df[df["Status usuario"] != "NOEJ"]
+        
     except UnicodeDecodeError as e:
         st.error("Error de decodificación. Intente cambiar la codificación del archivo si el problema persiste.")
         raise e
@@ -32,6 +41,17 @@ if archivo_usuario is not None:
         st.write(df)
 
         # Notificación de carga completada
-        st.success("Archivo cargado exitosamente!")
+        st.success("Archivo cargado y procesado exitosamente!")
+
+        # Preparar el DataFrame para la descarga
+        csv = df.to_csv(index=False, encoding='ISO-8859-1', sep=';').encode('ISO-8859-1')
+        
+        # Widget de descarga
+        st.download_button(
+            label="Descargar datos como CSV",
+            data=csv,
+            file_name='datos_procesados.csv',
+            mime='text/csv',
+        )
 else:
     st.write("Por favor, cargue un archivo CSV para continuar.")
